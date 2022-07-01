2587909760efd7087a00b39b3d5d1cfc5e7bc4f3
Kevin Maldonado-Cota <k_maldonado1@u.pacific.edu>
2022-06-30 22:16:05 -0700

    Stuff
    Adds bombs to arrays and has a print feature

--- TestFile.py
+++ TestFile.py
@@ -1,54 +1,51 @@
 #Test File
 
 import random
-#seed(1)
+def JefIsGay():
+	#Making the function to increment the values around the bomb
+	#dont duplicate code
+	#heeheeeeeeeereeee
+
 def main():
 	name = input("Welcome to Mine Sweeper!\nWhat is your name? ")
 	height = inputDimension("height")
 	width = inputDimension("width")
 	print("Creating a", height, " x ", width, "board! ")
 
-	grid = main_array(width, height)
-	#At this point array0 contains all 0s
-	#---------------------------------------
-	bomb_amount = width * height / 8 - 10
-	print("Hi")
-	add_bombs(bomb_amount, grid, height, width)
+	grid = initialize_Array(width, height)
 
-	#print_board(array0, height)
+	bomb_amount = (width * height) / 8
+
+	grid = add_bombs(bomb_amount, grid, height, width)
+
+	print_board(grid, height, width)
 
 
 def inputDimension(measurement):
-
 	returnMeasurement = input(f"What would you like the {measurement} of the board to be? ")
-
 	while(returnMeasurement.isnumeric() == False):
 		returnMeasurement = input(f"ERROR, ENTER A VALID POSITIVE INT!\nWhat would you like the {measurement} of the board to be? ")
 
 	return int(returnMeasurement)
 
-def main_array(w, h):
-	#arr = [[0] * int(w)] * int(h)
+def initialize_Array(w, h):
 	arr = [[0 for x in range(w)] for y in range(h)]
 	return arr
 
 def add_bombs(bombs, arr, h, w):
-	print("While")
-	while(bombs != 0):
-		print("Random")
-		a = random.randint(0, h - 1)
-		print("A: ", a)
-		b = random.randint(0, w - 1)
-		print("B: ", b)
+	while(bombs > 0):
+		random.seed(bombs)
+		a = random.randrange(h - 1)
+		b = random.randrange(w - 1)
 		if ((arr[a][b] != -1) ):
 			arr[a][b] = -1
-		bombs -=1
-	#return arr
+			bombs -=1
+	return arr
 		
 	
-
-def print_board(arr, height):
-	print(arr[0])
+def print_board(arr, height, width):
+	for height in arr:
+		print("".join(["{:<{mx}}".format(ele, mx = width) for ele in height]))
 
 	
 main()
