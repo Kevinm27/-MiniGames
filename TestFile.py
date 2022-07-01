78cc6dfcca0408ee11c00cd7d89292b0e50e7343
Felipe-Ma <70176706+Felipe-Ma@users.noreply.github.com>
2022-06-30 17:27:42 -0700

    Update TestFile.py
    Simple Input from user (width + height)

--- TestFile.py
+++ TestFile.py
@@ -3,11 +3,27 @@
 
 
 def main():
-	print("Hello")
-	val = input("Enter your value: ")
-	print(val)
+	name = input("Welcome to Mine Sweeper! What is your name? ")
+	height = inputDimension("height")
+	width = inputDimension("width")
+
+<<<<<<< Updated upstream
 
 
 
+=======
+	print("Creating a", str(height), " x ", str(width), "board!")
 
+def inputDimension(measurement):
+	print("What would you like the", measurement, "of the board to be?")
+	returnMeasurement = input()
+
+	try:
+		returnMeasurement = int(returnMeasurement)
+		return returnMeasurement
+	except:
+		print("Please enter a number!")
+		inputDimension(measurement)
+	
+>>>>>>> Stashed changes
 main()
