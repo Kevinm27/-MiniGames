b1172c55ad5fcb2fafa6630d84ff69f95d678848
Kevin Maldonado-Cota <k_maldonado1@u.pacific.edu>
2022-06-30 21:47:51 -0700

    Buggy Code Freezes atm

--- TestFile.py
+++ TestFile.py
@@ -1,50 +1,54 @@
 #Test File
 
 import random
-
-
-
+#seed(1)
 def main():
 	name = input("Welcome to Mine Sweeper!\nWhat is your name? ")
 	height = inputDimension("height")
 	width = inputDimension("width")
-	print("Creating a", str(height), " x ", str(width), "board! ")
+	print("Creating a", height, " x ", width, "board! ")
 
-	array0 = main_array(width, height)
+	grid = main_array(width, height)
+	#At this point array0 contains all 0s
+	#---------------------------------------
+	bomb_amount = width * height / 8 - 10
+	print("Hi")
+	add_bombs(bomb_amount, grid, height, width)
 
-	bomb_amount = int(width) * int(height)/8 -10
-
-	array0 = add_bombs(bomb_amount, array0, height, width)
-
-	print_board(array0, height)
+	#print_board(array0, height)
 
 
 def inputDimension(measurement):
 
-	returnMeasurement = input(f" What would you like the {measurement} of the board to be? ")
+	returnMeasurement = input(f"What would you like the {measurement} of the board to be? ")
 
 	while(returnMeasurement.isnumeric() == False):
-		returnMeasurement = input(f" ERROR, ENTER A VALID POSITIVE INT!\nWhat would you like the {measurement} of the board to be? ")
-	return returnMeasurement
+		returnMeasurement = input(f"ERROR, ENTER A VALID POSITIVE INT!\nWhat would you like the {measurement} of the board to be? ")
+
+	return int(returnMeasurement)
 
 def main_array(w, h):
-	arr = [[0] * int(w)] * int(h)
+	#arr = [[0] * int(w)] * int(h)
+	arr = [[0 for x in range(w)] for y in range(h)]
 	return arr
 
 def add_bombs(bombs, arr, h, w):
+	print("While")
 	while(bombs != 0):
-		a = random.randrange(int(h) - 1)
-		b = random.randrange(int(w) - 1)
+		print("Random")
+		a = random.randint(0, h - 1)
+		print("A: ", a)
+		b = random.randint(0, w - 1)
+		print("B: ", b)
 		if ((arr[a][b] != -1) ):
 			arr[a][b] = -1
-			bombs -=1
-	return arr
+		bombs -=1
+	#return arr
 		
 	
 
 def print_board(arr, height):
-	for height in arr:
-		print(height)
+	print(arr[0])
 
 	
 main()
