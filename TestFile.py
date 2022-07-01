c6eedff18187d332b3347aed3297f4fe2a2becb3
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-06-30 21:25:57 -0700

    6/30 9:25 pm

--- TestFile.py
+++ TestFile.py
@@ -1,5 +1,7 @@
 #Test File
 
+import random
+
 
 
 def main():
@@ -9,6 +11,12 @@ def main():
 	print("Creating a", str(height), " x ", str(width), "board! ")
 
 	array0 = main_array(width, height)
+
+	bomb_amount = int(width) * int(height)/8 -10
+
+	array0 = add_bombs(bomb_amount, array0, height, width)
+
+	print_board(array0, height)
 
 
 def inputDimension(measurement):
@@ -20,7 +28,23 @@ def inputDimension(measurement):
 	return returnMeasurement
 
 def main_array(w, h):
-	arr = [[0] * w] * h
+	arr = [[0] * int(w)] * int(h)
 	return arr
+
+def add_bombs(bombs, arr, h, w):
+	while(bombs != 0):
+		a = random.randrange(int(h) - 1)
+		b = random.randrange(int(w) - 1)
+		if ((arr[a][b] != -1) ):
+			arr[a][b] = -1
+			bombs -=1
+	return arr
+		
+	
+
+def print_board(arr, height):
+	for height in arr:
+		print(height)
+
 	
 main()
