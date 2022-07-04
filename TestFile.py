2bf756d81138ae62c72b1a2a782a380ca6077dbc
Kevin Maldonado-Cota <92879491+Kevinm27@users.noreply.github.com>
2022-07-03 23:44:56 -0700

    Added Actual Random Gen
    Text file increments on every run
    This file is used as a seed
    Might change the algorithm for incrementing around bombs.

--- TestFile.py
+++ TestFile.py
@@ -2,7 +2,6 @@
 
 import random
 
-
 def numbers(array, width, height):
 	
 	for i in range(width):
@@ -35,17 +34,26 @@ def numbers(array, width, height):
 	
 	return array
 			
+#Instead of the numbers function above, we could probably make it a lil better
+#Im thinking of storing the random bombs into an array with their coordinates
+	# - In these coordinates, we add one to the numbers around it
+'''def incrementSurrounding(array, rows, cols, bomb_Locations, num_Bombs):
+	for i in range(num_Bombs):
+		#Check all use cases, including the sides and corners
+'''		
 
-
-
-
-			
-
-	#Making the function to increment the values around the bomb
-	#dont duplicate code
-	#heeheeeeeeeereeee
 
 def main():
+	#Making seed actually random
+	fileRead = open("seed.txt", "r")
+	num = int(fileRead.read())
+	SEED = num
+	fileRead.close
+
+	fileOverwrite = open("seed.txt", "w")
+	fileOverwrite.write(str(num + 1))
+	fileOverwrite.close
+
 	name = input("Welcome to Mine Sweeper!\nWhat is your name? ")
 	height = inputDimension("height")
 	width = inputDimension("width")
@@ -55,17 +63,19 @@ def main():
 
 	bomb_amount = (width * height) / 8
 
-	grid = add_bombs(bomb_amount, grid, height, width)
+	grid, bomb_Locations = add_bombs(bomb_amount, grid, height, width, SEED)
 
 	grid = numbers(grid, width, height)
 
 	print_board(grid, height, width)
 
+	print(bomb_Locations)
+
 
 def inputDimension(measurement):
 	returnMeasurement = input(f"What would you like the {measurement} of the board to be? ")
-	while(returnMeasurement.isnumeric() == False):
-		returnMeasurement = input(f"ERROR, ENTER A VALID POSITIVE INT!\nWhat would you like the {measurement} of the board to be? ")
+	while(returnMeasurement.isnumeric() == False or int(returnMeasurement) <= 3):
+		returnMeasurement = input(f"ERROR, ENTER A VALID INT!(> 3)\nWhat would you like the {measurement} of the board to be? ")
 
 	return int(returnMeasurement)
 
@@ -73,15 +83,18 @@ def initialize_Array(width, height):
 	array = [[0 for x in range(width)] for y in range(height)]
 	return array
 
-def add_bombs(bombs, array, height, width):
+def add_bombs(bombs, array, height, width, SEED):
+	bomb_Locations = []
 	while(bombs > 0):
-		random.seed(bombs)
+		random.seed(bombs + SEED)
 		a = random.randrange(height - 1)
+		random.seed(bombs + SEED + 1)
 		b = random.randrange(width - 1)
 		if ((array[a][b] != -1) ):
+			bomb_Locations.append((a, b))
 			array[a][b] = -1
 			bombs -=1
-	return array
+	return array, bomb_Locations
 		
 	
 def print_board(arr, height, width):
